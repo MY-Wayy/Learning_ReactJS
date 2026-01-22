def3ef50b0754211173eea7b3684a6bbf88d8036
MY-Wayy <rlfalsgur316@naver.com>
2026-01-22 16:39:38 +0900

    ch6.4 까지 수강-최종 (2026.1.22)

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -5,3 +5,7 @@ import App2 from "./Ch6_4_App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App2 />);
+
+//root.render() 안에
+//chapter 6.3 까지는 <App />
+//chapter 6.4 는 <App2 />
