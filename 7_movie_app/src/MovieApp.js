ef465c8c0d7a6e3d79e8f0020dac0a44952f6e69
MY-Wayy <rlfalsgur316@gmail.com>
2026-01-29 21:11:24 +0900

    7.3 수강 중 - 개요까지 해결 (2026.1.29)

--- 7_movie_app/src/MovieApp.js
+++ 7_movie_app/src/MovieApp.js
@@ -5,9 +5,10 @@ const TMDB_TOKEN = process.env.REACT_APP_TMDB_TOKEN;
 function MovieApp() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const [jenres, setJenres] = useState([]);
 
   //TMDB API 사용
-  const url =
+  const moviesUrl =
     "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
   const options = {
     method: "GET",
@@ -16,22 +17,34 @@ function MovieApp() {
       Authorization: `Bearer ${TMDB_TOKEN}`,
     },
   };
+  const jenresUrl =
+    "https://api.themoviedb.org/3/genre/movie/list?language=ko-KR";
 
   // async-await 사용을 위한 함수
   // .then() 문법 대체 (일반적임)
   const getMovies = async () => {
     // 강의 코드 작동 안 함 -> TMDB API 사용
-    const json = await (await fetch(url, options)).json();
+    const json = await (await fetch(moviesUrl, options)).json();
     setMovies(json.results);
+    setLoading(false);
+  };
+  const getJenres = async () => {
+    // 강의 코드 작동 안 함 -> TMDB API 사용
+    const json = await (await fetch(jenresUrl, options)).json();
+    setJenres(json.results);
     setLoading(false);
   };
 
   useEffect(() => {
     getMovies();
+    getJenres();
   }, []);
 
+  console.log("movies");
   console.log(movies);
-
+  console.log("jenres");
+  console.log(jenres);
+  //TODO: 장르 구현
   return (
     <div>
       {loading ? (
